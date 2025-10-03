5b9f58ade207135d08abc61125169fd27c0a37b0
Ignaciobaquero <ignaciobaquero023@gmail.com>
2025-10-03 04:46:25 -0300

    CreaTuLanding1-Baquero

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,13 +1,13 @@
 import styles from '../components/ItemListContainer.module.css'
 
 
-const ItemListContainer = ({ bienvenido}) => {
+const ItemListContainer = ({bienvenido}) => {
 
 
  return (
    <main>
     <div className={styles.bienvenido}>
-    <h1 style={{ color: "white" }}>Bienvenido Usuario</h1>
+    <h1 style={{ color: "white " }}>Bienvenido Usuario</h1>
     </div>
    </main>
 
